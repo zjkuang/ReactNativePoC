f623ba1c2f2930d41b161be12f8f01e99c9089e4
John Kuang <John.Kuang@pditechnologies.com>
2025-02-06 18:02:17 -0600

    cleaning up

--- android/app/src/main/java/MicroBlinkNativeModule/MicroBlinkNativeModule.java
+++ android/app/src/main/java/MicroBlinkNativeModule/MicroBlinkNativeModule.java
@@ -1,17 +1,14 @@
 package MicroBlinkNativeModule;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableMap;
 
 public class MicroBlinkNativeModule extends ReactContextBaseJavaModule {
     private static ReactApplicationContext reactApplicationContextContext;
